61196a30c44e88ca4642260596feb551de8113b7
Gabcampos23 <gab.campos@duocuc.cl>
2023-06-22 23:24:12 -0400

    Se corrigen funcioanlidades en Api Ventas

--- controllers/Ventas.js
+++ controllers/Ventas.js
@@ -5,25 +5,47 @@ const app = express();
 
 // Agregar una nueva venta
 module.exports.agregar = app.post('/', (req, res) => {
-    const { id_ventas, fecha, estado, hora, id_usuario } = req.body;
-    const sql = "INSERT INTO VENTAS (id_venta, fecha, estado, hora, id_usuario) VALUES (?, ?, ?, ?, ?)";
-    const values = [id_ventas, fecha, estado, hora, id_usuario];
-
+    const {  FECHA, HORA, MONTO_TOTAL_VENTA, FECHA_DESPACHO,FECHA_ENTREGA } = req.body;
+    const sql = `INSERT INTO VENTAS ( 
+        FECHA, 
+        ESTADO,
+        HORA,
+        MONTO_TOTAL_VENTA) 
+        VALUES (?, 1, ?, ?)
+        `;
+    const values = [ FECHA, HORA, MONTO_TOTAL_VENTA];
+    let ID_VENTA = 0;
     connection.query(sql, values, (error, results) => {
         if (error) throw error;
-        res.status(200).send('Venta agregada exitosamente');
+        ID_VENTA = results.insertId;
+        res.status(200).send(`Venta agregada exitosamente con id ${results.insertId}`);
     });
+    let sql_despacho = `
+    INSERT INTO DESPACHOS (
+        FECHA_DESPACHO, 
+        FECHA_ENTREGA, 
+        ID_SEGUIMIENTO,
+         ID_VENTA)
+         VALUES (?,?,1,?)`;
+         
+    let values_despacho = [ FECHA_DESPACHO, FECHA_ENTREGA, ID_VENTA]
+    connection.query(sql_despacho, values_despacho,(error,results) => {
+        if (error) throw error;
+
+        response.status(200).send(`Despacho registrado correctamente con id ${results.insertId}`);
 });
+});
+
 
 
 // Obtener todas las ventas
 module.exports.buscar_todo = app.patch('/', (request, response) => {
     const sql = `SELECT 
-                id_venta,
-                fecha,
-                estado,
-                hora,
-                id_usuario 
+                ID_VENTA,
+                FECHA,
+                ESTADO,
+                HORA,
+                MONTO_TOTAL_VENTA
                 FROM VENTAS`;
     connection.query(sql, (error, results) => {
         if (error) throw error;
@@ -38,51 +60,33 @@ module.exports.buscar_todo = app.patch('/', (request, response) => {
 
 // Actualizar una venta
 module.exports.actualizar = app.patch('/', (req, res) => {
-    const { id_ventas, fecha, estado, hora, id_usuario } = req.body;
-    const sql = `UPDATE VENTAS SET fecha = ?,
-                                    estado = ?,
-                                    hora = ?, 
-                                    id_usuario = ? 
+    const { ID_VENTA, FECHA, ESTADO, HORA, MONTO_TOTAL_VENTA } = req.body;
+    const sql = `UPDATE VENTAS SET FECHA = ?,
+                                    ESTADO = ?,
+                                    HORA = ?,
+                                    MONTO_TOTAL_VENTA = ?,  
                                     WHERE 
-                                    id_venta = ?`;
-    const values = [fecha, estado, hora, id_usuario, id_ventas];
+                                    ID_VENTA = ?`;
+    const values = [FECHA, ESTADO, HORA, MONTO_TOTAL_VENTA, ID_VENTA];
 
     connection.query(sql, values, (error, results) => {
         if (error) throw error;
-        res.send(`Venta con id ${id_ventas} actualizada correctamente`);
-    });
-});
-
-// Agregar una nueva venta
-module.exports.agregar = app.post('/', (req, res) => {
-    const { id_ventas, fecha, estado, hora, id_usuario } = req.body;
-    const sql = `INSERT INTO VENTAS
-                            (id_venta,
-                            fecha,
-                            estado,
-                            hora,
-                            id_usuario) 
-                            VALUES (?, ?, ?, ?, ?)`;
-    const values = [id_ventas, fecha, estado, hora, id_usuario];
-
-    connection.query(sql, values, (error, results) => {
-        if (error) throw error;
-        res.status(200).send('Venta agregada exitosamente');
+        res.send(`Venta con id ${ID_VENTA} actualizada correctamente`);
     });
 });
 
 // Eliminar una venta
 module.exports.eliminar = app.delete('/', (request, response) => {
-    const { id_ventas } = request.body;
+    const { ID_VENTA } = request.body;
     const sql = `UPDATE FROM VENTAS 
                 SET ESTADO = 0 
-                WHERE id_venta = ?`;
-    connection.query(sql, id_ventas, (error, results) => {
+                WHERE ID_VENTA = ?`;
+    connection.query(sql, ID_VENTA, (error, results) => {
         if (error) throw error;
         if (results.affectedRows > 0) {
-            response.status(200).send(`Venta con id ${id_ventas} eliminada correctamente`);
+            response.status(200).send(`Venta con id ${ID_VENTA} eliminada correctamente`);
         } else {
-            response.status(404).send(`Venta con id ${id_ventas} no encontrada`);
+            response.status(404).send(`Venta con id ${ID_VENTA} no encontrada`);
         }
     });
 });
