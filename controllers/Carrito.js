e64e0512bed7ee4601f61d01f551265c93ad1e15
MatiasOlivac <mati.oliva@duocuc.cl>
2023-07-05 17:34:36 -0400

    Carrito/id

--- controllers/Carrito.js
+++ controllers/Carrito.js
@@ -32,6 +32,34 @@ module.exports.buscar_todo = app.get('/', (request, response)=> {
     });
 });
 
+//Metodo GET por ID
+
+module.exports.buscar = app.get('/:id', (request, response) => {  
+    const id_carrito = request.params.id;
+    const sql = `
+    SELECT 
+        ID_CARRITO,
+        IMAGEN, 
+        CANTIDAD,
+        PRECIO,
+        SUBTOTAL,
+        ESTADO,
+    FROM CARRITO 
+    WHERE ID_CARRITO = ?
+     `;
+    connection.query(sql, id_carrito, (error, results) => {
+        if (error) throw error;
+        if (results.length > 0) {
+            response.status(200).send(results);
+        } else {
+            response.status(204).send('Sin resultado');
+        }
+    });               
+});
+
+
+
+
 //Metodo PATCH
 module.exports.actualizar = app.put('/', (request, response) =>{
     const {
@@ -104,6 +132,8 @@ module.exports.agregar = app.post('/', (request, response) => {
     });
 });
 
+
+//Metodo DELETE
 module.exports.eliminar = app.delete('/:id', (request, response) => {
     const ID_CARRITO = request.params.id;
 
