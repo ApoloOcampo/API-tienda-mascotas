639ac3149fcd77328e0c36d8410861d89caebabb
Leonardo Pavez <107446587+DSLeonardoPavez@users.noreply.github.com>
2023-07-14 17:58:28 -0400

    Actualización de las  API
    Suscripcion promociones Donaciones

--- controllers/Donacion.js
+++ controllers/Donacion.js
@@ -5,68 +5,73 @@ const app = express();
 
 // Agregar una nueva donación
 module.exports.agregar = app.post('/', (req, res) => {
-    const { id_donacion, fecha, monto, id_usuario } = req.body;
-    const sql = `INSERT INTO DONACIONES (
-                        id_donacion, 
-                        fecha, monto, 
-                        id_usuario) 
-                        VALUES (?, ?, ?, ?)`;
-    const values = [id_donacion, fecha, monto, id_usuario];
-
+    const { ID_DONACION, FECHA, MONTO, ID_USUARIO, ESTADO } = req.body;
+    const sql = `INSERT INTO DONACIONES 
+                (ID_DONACION, 
+                FECHA, MONTO,
+                ID_USUARIO, 
+                ESTADO) 
+                VALUES (?, ?, ?, ?, ?)`;
+    const values = [ID_DONACION, FECHA, MONTO, ID_USUARIO, ESTADO];
+  
     connection.query(sql, values, (error, results) => {
-        if (error) throw error;
-        res.status(200).send('Donación agregada exitosamente');
+      if (error) throw error;
+      res.status(200).send('Donación agregada exitosamente');
     });
-});
+  });
 
 
 // Obtener todas las donaciones
 module.exports.buscar_todo = app.patch('/', (request, response) => {
     const sql = `SELECT 
-                id_donacion,
-                fecha, monto,
-                id_usuario 
-                FROM DONACIONES`;
+            ID_DONACION,
+            FECHA, 
+            MONTO,
+            ID_USUARIO, 
+            ESTADO 
+            FROM DONACIONES`;
     connection.query(sql, (error, results) => {
-        if (error) throw error;
-        if (results.length > 0) {
-            response.status(200).send(results);
-        } else {
-            response.status(204).send('Sin resultado');
-        }
+      if (error) throw error;
+      if (results.length > 0) {
+        response.status(200).send(results);
+      } else {
+        response.status(204).send('Sin resultado');
+      }
     });
-});
+  });
 
 
 // Actualizar una donación
 module.exports.actualizar = app.patch('/', (req, res) => {
-    const { id_donacion, fecha, monto, id_usuario } = req.body;
-    const sql = `UPDATE DONACIONES SET 
-                        fecha = ?,
-                        monto = ?,
-                        id_usuario = ? 
-                        WHERE id_donacion = ?`;
-    const values = [fecha, monto, id_usuario, id_donacion];
-
+    const { ID_DONACION, FECHA, MONTO, ID_USUARIO, ESTADO } = req.body;
+    const sql = `UPDATE DONACIONES
+                SET FECHA = ?,
+                MONTO = ?,
+                ID_USUARIO = ?,
+                ESTADO = ?
+                WHERE ID_DONACION = ?`;
+    const values = [FECHA, MONTO, ID_USUARIO, ESTADO, ID_DONACION];
+  
     connection.query(sql, values, (error, results) => {
-        if (error) throw error;
-        res.send(`Donación con id_donacion ${id_donacion} actualizada correctamente`);
+      if (error) throw error;
+      res.send(`Donación con ID_DONACION ${ID_DONACION} actualizada correctamente`);
     });
-});
+  });
 
 
 // Eliminar una donación
-module.exports.eliminar = app.delete('/', (request, response) => {
-    const { id_donacion } = request.body;
-    const sql = `UPDATE FROM DONACIONES
-                SET ESTADO = 0  
-                WHERE id_donacion = ?`;
-    connection.query(sql, id_donacion, (error, results) => {
-        if (error) throw error;
-        if (results.affectedRows > 0) {
-            response.status(200).send(`Donación con id ${id_donacion} eliminada correctamente`);
-        } else {
-            response.status(404).send(`Donación con id ${id_donacion} no encontrada`);
-        }
+module.exports.eliminar = app.patch('/', (request, response) => {
+    const { ID_DONACION } = request.body;
+    const sql = `UPDATE 
+                DONACIONES 
+                SET ESTADO = 0 
+                WHERE ID_DONACION = ?`;
+    connection.query(sql, ID_DONACION, (error, results) => {
+      if (error) throw error;
+      if (results.affectedRows > 0) {
+        response.status(200).send(`Donación con ID ${ID_DONACION} eliminada correctamente`);
+      } else {
+        response.status(404).send(`Donación con ID ${ID_DONACION} no encontrada`);
+      }
     });
-});
+  });
--- controllers/Suscripcion.js
+++ controllers/Suscripcion.js
@@ -5,68 +5,68 @@ const app = express();
 
 // Método POST para agregar una suscripción
 module.exports.agregar = app.post('/', (req, res) => {
-    const { ID_SUSCRIPTOR, FECHA_INICIO, ESTADO_SUSCRIPCION, FECHA_TERMINO, ID_USUARIO, MONTO } = req.body;
-    const sql = `INSERT INTO SUSCRIPCION 
-                (ID_SUSCRIPTOR, FECHA_INICIO, ESTADO_SUSCRIPCION, FECHA_TERMINO, ID_USUARIO, MONTO) 
-                VALUES (?, ?, ?, ?, ?, ?)`;
-    const values = [ID_SUSCRIPTOR, FECHA_INICIO, ESTADO_SUSCRIPCION, FECHA_TERMINO, ID_USUARIO, MONTO];
-  
-    connection.query(sql, values, (error, results) => {
-      if (error) throw error;
-      res.status(200).send('Suscripción agregada exitosamente');
-    });
+  const { ID_SUSCRIPTOR, FECHA_INICIO, ESTADO, FECHA_TERMINO, ID_USUARIO, MONTO } = req.body;
+  const sql = `INSERT INTO SUSCRIPCION 
+              (ID_SUSCRIPTOR, FECHA_INICIO, ESTADO, FECHA_TERMINO, ID_USUARIO, MONTO) 
+              VALUES (?, ?, ?, ?, ?, ?)`;
+  const values = [ID_SUSCRIPTOR, FECHA_INICIO, ESTADO, FECHA_TERMINO, ID_USUARIO, MONTO];
+
+  connection.query(sql, values, (error, results) => {
+    if (error) throw error;
+    res.status(200).send('Suscripción agregada exitosamente');
   });
-  
-  // Método GET para buscar todas las suscripciones
-  module.exports.buscar_todo = app.get('/', (request, response) => {
-    const sql = `SELECT 
-                ID_SUSCRIPTOR, 
-                FECHA_INICIO,
-                ESTADO_SUSCRIPCION,
-                FECHA_TERMINO,
-                ID_USUARIO,
-                MONTO
-                FROM SUSCRIPCION`;
-    connection.query(sql, (error, results) => {
-      if (error) throw error;
-      if (results.length > 0) {
-        response.status(200).send(results);
-      } else {
-        response.status(204).send('Sin resultado');
-      }
-    });
+});
+
+// Método GET para buscar todas las suscripciones
+module.exports.buscar_todo = app.get('/', (request, response) => {
+  const sql = `SELECT 
+              ID_SUSCRIPTOR, 
+              FECHA_INICIO,
+              ESTADO,
+              FECHA_TERMINO,
+              ID_USUARIO,
+              MONTO
+              FROM SUSCRIPCION`;
+  connection.query(sql, (error, results) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      response.status(200).send(results);
+    } else {
+      response.status(204).send('Sin resultado');
+    }
   });
-  
-  // Método PATCH para actualizar una suscripción
-  module.exports.actualizar = app.patch('/', (req, res) => {
-    const { ID_SUSCRIPTOR, FECHA_INICIO, ESTADO_SUSCRIPCION, FECHA_TERMINO, ID_USUARIO, MONTO } = req.body;
-    const sql = `UPDATE SUSCRIPCION SET 
-                FECHA_INICIO = ?,
-                ESTADO_SUSCRIPCION = ?,
-                FECHA_TERMINO = ?,
-                ID_USUARIO = ?,
-                MONTO = ?
-                WHERE ID_SUSCRIPTOR = ?`;
-    const values = [FECHA_INICIO, ESTADO_SUSCRIPCION, FECHA_TERMINO, ID_USUARIO, MONTO, ID_SUSCRIPTOR];
-  
-    connection.query(sql, values, (error, results) => {
-      if (error) throw error;
-      res.send(`Suscripción con ID ${ID_SUSCRIPTOR} actualizada correctamente`);
-    });
+});
+
+// Método PATCH para actualizar una suscripción
+module.exports.actualizar = app.patch('/:idSuscripcion', (req, res) => {
+  const { ID_SUSCRIPTOR, FECHA_INICIO, ESTADO, FECHA_TERMINO, ID_USUARIO, MONTO } = req.body;
+  const sql = `UPDATE SUSCRIPCION SET 
+              FECHA_INICIO = ?,
+              ESTADO = ?,
+              FECHA_TERMINO = ?,
+              ID_USUARIO = ?,
+              MONTO = ?
+              WHERE ID_SUSCRIPTOR = ?`;
+  const values = [FECHA_INICIO, ESTADO, FECHA_TERMINO, ID_USUARIO, MONTO, ID_SUSCRIPTOR];
+
+  connection.query(sql, values, (error, results) => {
+    if (error) throw error;
+    res.send(`Suscripción con ID ${ID_SUSCRIPTOR} actualizada correctamente`);
   });
-  
-  // Método PATCH para actualizar estado de una suscripción
-  module.exports.eliminar = app.patch('/', (request, response) => {
-    const { ID_SUSCRIPTOR } = request.body;
-    const sql = `UPDATE SUSCRIPCION 
-                 SET ESTADO_SUSCRIPCION = 0 
-                 WHERE ID_SUSCRIPTOR = ?`;
-    connection.query(sql, ID_SUSCRIPTOR, (error, results) => {
-      if (error) throw error;
-      if (results.affectedRows > 0) {
-        response.status(200).send(`Suscripción con ID ${ID_SUSCRIPTOR} eliminada correctamente`);
-      } else {
-        response.status(404).send(`Suscripción con ID ${ID_SUSCRIPTOR} no encontrada`);
-      }
-    });
+});
+
+// Método PATCH para actualizar estado de una suscripción
+module.exports.eliminar = app.patch('/:idSuscripcion', (request, response) => {
+  const { ID_SUSCRIPTOR } = request.body;
+  const sql = `UPDATE SUSCRIPCION 
+               SET ESTADO = 0 
+               WHERE ID_SUSCRIPTOR = ?`;
+  connection.query(sql, ID_SUSCRIPTOR, (error, results) => {
+    if (error) throw error;
+    if (results.affectedRows > 0) {
+      response.status(200).send(`Suscripción con ID ${ID_SUSCRIPTOR} eliminada correctamente`);
+    } else {
+      response.status(404).send(`Suscripción con ID ${ID_SUSCRIPTOR} no encontrada`);
+    }
   });
+});
